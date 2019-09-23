8391390c138047c071a4055b569d28d0b038e7e5
1robo <34087593+1robo@users.noreply.github.com>
2019-09-23 23:15:03 +0800

    Create code1_2.py

---
+++ Day1/code1_2.py
@@ -0,0 +1,37 @@
+### Day1 住房贷款 图表1 ###
+
+import matplotlib.pyplot as plt
+
+A = 4000000     #总贷款额
+Y = 10          #贷款年份
+C = C1 = 0      #等额本息、等额本金总还款
+B = B1 = 0      #等额本息、等额本金总利息  
+R = 0.06        #年利率 
+m = R/12        #月利率
+n = 12*Y        #总还款期数
+d = A/n         #等额本金每月本金还款
+
+#print("### 等额本息",Y,"年 ###")     #等额本息
+#C = A*R*Y*pow(1+m,n) / (pow(1+m,n)-1)
+
+#print("### 等额本金",Y,"年 ###")     #等额本金 
+#C1 = A*(2+m*n+m)/2
+
+A = int(A/10000)                #贷款400万
+pic_x = [1,5,10,15,20,25,30]    # 年份
+pic_y = [A*R*i*pow(1+m,i*12)/(pow(1+m,i*12)-1) for i in pic_x] # 万元
+pic_y1= [A*(2+m*i*12+m)/2 for i in pic_x]
+
+# 通用字体设置
+import matplotlib as mpl
+mpl.rcParams['font.sans-serif'] = ['SimHei']   #设置简黑字体
+mpl.rcParams['axes.unicode_minus'] = False # 解决‘-’bug
+
+title = str(A) + '万贷款 年利率'+str(int(R*100))+'% 总还款额示意图\n蓝色-等额本息 黄色-等额本金'
+plt.title(title)
+plt.xlabel('贷款年份')
+plt.ylabel('总还款额（万元）')
+plt.plot(pic_x,pic_y)
+plt.plot(pic_x,pic_y1)
+plt.grid(linestyle='-.')
+plt.show()
