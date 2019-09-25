9079649bed83c5f0398eea8f7a9bc6a413d8a3c1
1robo <34087593+1robo@users.noreply.github.com>
2019-09-25 17:25:16 +0800

    Update code2_1.py

--- Day2/code2_1.py
+++ Day2/code2_1.py
@@ -4,7 +4,7 @@ import numpy as np
 # 通用字体设置
 import matplotlib as mpl
 mpl.rcParams['font.sans-serif'] = ['SimHei']   #设置简黑字体
-
+mpl.rcParams['axes.unicode_minus'] = False # 解决‘-’bug
 plt.grid(linestyle='-.')
 title = '弹簧伸长与所吊重物质量之间关系模型'
 plt.title(title)
@@ -18,15 +18,13 @@ print(F) #输出各项系数
 P = np.poly1d(F)
 print(P) #输出方程式
 
-print(np.polyval(F,600))	#输出重量为600g的长度
+print(P(600))	#输出重量为600g的长度
 
 
-M1 = [ 50*i for i in range(1,16)] 
-L1 = np.polyval(F,M1)
+M_New = M + [50*i for i in range(12,15)]  #延长 x 轴
 plt.plot(M,L,'o',label='实际测量值')
-plt.plot(M1,L1,label='拟合曲线')
+plt.plot(M_New,P(M_New),label='拟合曲线')
 
-L2 = np.polyval(F,600)
-plt.plot(600,L2,'o',label='预测值')
+plt.plot(600,P(600),'o',label='预测值')
 plt.legend() 
 plt.show()
