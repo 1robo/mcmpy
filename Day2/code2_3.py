2c455193d2b92064fd3eba29934acfac0b460c0e
1robo <34087593+1robo@users.noreply.github.com>
2019-09-25 16:51:44 +0800

    Add files via upload

---
+++ Day2/code2_3.py
@@ -0,0 +1,34 @@
+### Day2 曲线拟合 回归分析 ###
+import matplotlib.pyplot as plt 
+import numpy as np
+# 通用字体设置
+import matplotlib as mpl
+mpl.rcParams['font.sans-serif'] = ['SimHei']   #设置简黑字体
+mpl.rcParams['axes.unicode_minus'] = False # 解决‘-’bug
+plt.grid(linestyle='-.')
+title = '酵母菌数量增长模型'
+plt.title(title)
+plt.xlabel('时间 (小时）')
+plt.ylabel('酵母菌数量（万个）')
+
+time = [i for i in range(0,19)]
+number = [9.6,18.3,29,47.2,71.1,119.1,174.6,257.3,\
+          350.7,441.0,513.3,559.7,594.8,629.4,640.8,\
+          651.1,655.9,659.6,661.8]
+
+#plt.plot(time,number)#画图
+#plt.show()#显示
+
+F = np.polyfit(time,number,1) #按一阶多项式拟合
+print(F) #输出各项系数
+P = np.poly1d(F)
+print(P) #输出方程式
+
+ 
+number1 = np.polyval(F,time)
+plt.plot(time,number,'o',label='实际测量值')
+plt.plot(time,number1,label='拟合曲线')
+plt.legend() 
+plt.show()
+
+
