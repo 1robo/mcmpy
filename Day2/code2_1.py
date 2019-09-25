e6473a5ab2d8ad62e145d59f4ec5a6ba542d1029
1robo <34087593+1robo@users.noreply.github.com>
2019-09-25 17:31:06 +0800

    Update code2_1.py

--- Day2/code2_1.py
+++ Day2/code2_1.py
@@ -13,18 +13,19 @@ plt.ylabel('弹簧伸长（厘米）')
 
 M = [ 50*i for i in range(1,12)] #列表解析表达式
 L = [6.005,6.879,7.750,8.252,9.370,9.872,10.675,11.502,12.365,13.120,13.805]
-F = np.polyfit(M,L,1) #按一阶多项式拟合
-print(F) #输出各项系数
-P = np.poly1d(F)
-print(P) #输出方程式
 
-print(P(600))	#输出重量为600g的长度
+fit = np.polyfit(M,L,1) #按一阶多项式拟合
+print(fit) #输出各项系数
+F = np.poly1d(fit)
+print(F) #输出方程式
+
+print(F(600))	#输出重量为600g的长度
 
 
 M_New = M + [50*i for i in range(12,15)]  #延长 x 轴
 plt.plot(M,L,'o',label='实际测量值')
-plt.plot(M_New,P(M_New),label='拟合曲线')
+plt.plot(M_New,F(M_New),label='拟合曲线')
 
-plt.plot(600,P(600),'o',label='预测值')
+plt.plot(600,F(600),'o',label='预测值')
 plt.legend() 
 plt.show()
