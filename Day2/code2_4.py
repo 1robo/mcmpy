d33d3bc4bea38460307ffb1ed0806c919873afbd
1robo <34087593+1robo@users.noreply.github.com>
2019-09-27 12:51:45 +0800

    Update code2_4.py

--- Day2/code2_4.py
+++ Day2/code2_4.py
@@ -1,6 +1,5 @@
 # 随堂练习  读取excel 表格
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import numpy as np
 # 通用字体设置
@@ -13,22 +12,16 @@ plt.title(title)
 plt.xlabel('机器数量 (台）')
 plt.ylabel('每天生产商品（万个)')
 
-
 df = pd.read_excel('work.xlsx')
-
 table =df.loc[:,['MACHINE','PRODUCT']].values
 
-m = []
-p = []
-for i in range(len(table)) :
-    m.append(table[i][0])
-    p.append(table[i][1])
+m = [table[i][0] for i in range(len(table))]
+p = [table[i][1] for i in range(len(table))]
 
 fit = np.polyfit(m,p,1)   #按一阶多项式拟合
 print(fit)                #输出各项系数
 F = np.poly1d(fit)
 print(F)                #输出方程式
-
 print(round(F(6)))             #6台机器产量
 print(round(F(25)))            #25台机器产量
 
@@ -37,7 +30,5 @@ p1 = F(m1)
 plt.plot(m,p,'o',label='实际测量值')
 plt.plot(m1,p1,label='拟合曲线')
 plt.plot([6,25],[F(6),F(25)],'o',label='预测值')
-
 plt.legend() 
 plt.show()
-
