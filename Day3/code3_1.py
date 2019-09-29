de3305a1545e1c358cb695fa6f524b790f0bb54b
1robo <34087593+1robo@users.noreply.github.com>
2019-09-29 19:53:02 +0800

    Update code3_1.py

--- Day3/code3_1.py
+++ Day3/code3_1.py
@@ -1,18 +1,10 @@
-### Day3 简单图论模型 范例程序2 ###
-
-# 更多的networkx用法
-# 参考官方文档 https://networkx.github.io/documentation/latest/tutorial.html
-
 import matplotlib.pyplot as plt
 import networkx as nx
 G = nx.Graph()
 nlist = ['A','B','C','D']
 G.add_nodes_from(nlist)
-G.add_edge('A', 'B',weight=1)
-G.add_edge('B', 'C',weight=1)
-G.add_edge('C', 'D',weight=1)
-G.add_edge('A', 'D',weight=1)
-G.add_edge(nlist[0],nlist[2])
+elist = [('A','B'),('B','C'),('C','D'),('A','D'),('A','C')]
+G.add_edges_from(elist)
 nx.draw_networkx(G)
 print(G.number_of_edges())
 print(nx.shortest_path(G, 'A', 'C', weight='weight'))   
